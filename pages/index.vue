828c4b18d707f473fe1244b1d1a3931f1cf4221f
yukihigaa <uky02220@gmail.com>
2020-05-20 23:42:44 +0900

    XML parse

--- pages/index.vue
+++ pages/index.vue
@@ -37,9 +37,22 @@ export default {
     }
   },
   async asyncData({ $axios }) {
-    const res = await $axios.$get('http://weather.livedoor.com/forecast/rss/primary_area.xml');
-    const citys = res.getElementsByTagName('city');
-    console.log(res);
+
+    // parseするためのパッケージを読み込み
+    var parser = require('fast-xml-parser');
+
+    // XML取得
+    const xml = await $axios.$get('http://weather.livedoor.com/forecast/rss/primary_area.xml');
+    
+    // parseするときのオプション定義（これを指定しなければ属性が取得できずNullになる）
+    const options = {
+      attributeNamePrefix : "", // Attribute名の先頭prefixを指定しない
+      ignoreAttributes : false // XMLのattributeを取るためにはtrue
+    }
+
+    // XMLをJSONにする
+    var jsonObj = parser.parse(xml, options);
+    console.log(jsonObj.rss.channel['ldWeather:source'].pref[0]);
   }
 }
 </script>
